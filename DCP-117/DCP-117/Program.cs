2e907417f5619922c0b221e8859e24374c100af6
aamir <wasi3aamir@gmail.com>
2019-01-21 15:32:52 +0600

    DCP-117 sum of nth numbers is accepted

---
+++ DCP-117/DCP-117/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCP_117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int i=0; i<T; i++)
+            {
+                long s = 0, num;
+                num = int.Parse(Console.ReadLine());
+                s = (num * (num+1) / 2);
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
